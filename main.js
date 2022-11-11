17150b5c9146237c1a9d75434fb33b92a7756e92
iimad <alisawiali70@gmail.com>
2022-11-11 10:42:03 +0100

    Math

---
+++ main.js
@@ -0,0 +1,44 @@
+// hier wechslt die - nach + 
+let m = -4 ;
+
+console.log(Math.abs(m))
+
+
+console.log('-------------------')
+
+let z = 20;
+console.log(Math.sqrt(z))
+
+console.log('----------pow---------')
+
+
+console.log(Math.pow(3,4))
+
+console.log('---------round----------')
+
+// hier brigt die integer nummer(round)....
+
+console.log(Math.round(10.3))
+console.log(Math.round(10.5))
+console.log(Math.round(10.9))
+
+
+
+console.log('---------ceil----------')
+//  zeit die höste zahl 
+let s = 12.3;
+
+let result1= s;
+console.log(Math.ceil(result1))
+
+console.log('-------floor------------')
+//   hier wird die kleine unteger zahl
+let se = 12.3;
+
+let result= se;
+console.log(Math.floor(result))
+
+
+
+
+
